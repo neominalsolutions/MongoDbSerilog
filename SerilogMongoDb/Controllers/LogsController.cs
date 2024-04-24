c4579164208ed1a1c1bdf26e7c2c0d7b8ceb218d
neominal.solutions <neominal.solutions@gmail.com>
2024-04-24 14:13:15 +0300

    Add project files.

---
+++ SerilogMongoDb/Controllers/LogsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SerilogMongoDb.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class LogsController : ControllerBase
+  {
+    private readonly ILogger<LogsController> logger;
+
+    public LogsController(ILogger<LogsController> logger)
+    {
+      this.logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Log()
+    {
+      logger.LogCritical("Uygulamada ciddi bir hata meydana geldi");
+
+      // Custom veriyi log'a bind ettik. Log Stash ile
+      logger.LogCritical("Uygulamada ciddi bir hata meydana geldi, {userId}", Guid.NewGuid().ToString());
+
+      return Ok();
+    }
+  }
+}
